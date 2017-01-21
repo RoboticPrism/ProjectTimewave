6cc22649814dbfef9493ddff43a876c974f69e7e
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-01-20 23:28:30 -0500

    Added Time Manager.  Added characters that listen to the time manager for time updates

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    public float moveSpeed = .01f;
+    private bool alive = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Listens for updates from the time controller
+    public void ReceiveCurrentTime(int current_seconds)
+    {
+        if (alive)
+        {
+            DoActions(current_seconds);
+        }
+    }
+
+    // Searchs player actions and runs actions if its time
+    public virtual void DoActions(int current_seconds)
+    {
+
+    }
+
+    // Moves the character to the given location
+    public void MoveToLocation(Vector3 targetLocation)
+    {
+        StartCoroutine(MoveToLocationEnum(this.gameObject, targetLocation));
+    }
+
+    protected IEnumerator MoveToLocationEnum(GameObject character, Vector3 targetLocation)
+    {
+        while (Vector3.Distance(this.transform.position, targetLocation) > 0.01f)
+        {
+            character.transform.position = Vector3.MoveTowards(character.transform.position, targetLocation, moveSpeed);
+            yield return null;
+        }
+        this.transform.position = targetLocation;
+        yield return null;
+    }
+
+    // Makes the character say something
+    public void WriteText(string characterText)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Characters/WilsonCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilsonCharacter : Character {
+
+    
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void DoActions(int current_seconds)
+    {
+        if (current_seconds == 2)
+        {
+            MoveToLocation(new Vector3(2, 0, 0));
+        }
+    }
+    
+
+}
