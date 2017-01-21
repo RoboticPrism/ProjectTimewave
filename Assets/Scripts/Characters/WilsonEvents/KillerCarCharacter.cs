0cf29c7ec2610bd88b15a50a9ac750b240bcb544
RoboticPrism <ryan92130@gmail.com>
2017-01-21 11:28:58 -0500

    Feature time system (#1)
    * Added dummy file
    
    * scientist front back sprites
    
    * Added Time Manager.  Added characters that listen to the time manager for time updates
    
    * Added action system, allows us to just set user timelines in their own unique start functions.  See Wilson's script for examples
    
    * Added method to change characters futures.  Added car to kill wilson with (R.I.P. Wilson)

---
+++ Assets/Scripts/Characters/WilsonEvents/KillerCarCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillerCarCharacter : Character {
+
+	// Use this for initialization
+	void Start () {
+        List<Action> beginning_timeline = new List<Action>();
+        beginning_timeline.Add(new MoveToLocation(new Vector3(0, 4, 0), 7));
+        ChangeFuture(beginning_timeline);
+    }
+	
+}
