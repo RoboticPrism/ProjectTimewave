63a5c29e1a0e5f4494373c6e05e00d0d863556e7
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-01-22 05:11:58 -0500

    Start of wilson's plot

--- Assets/Scripts/Jerry.cs
+++ Assets/Scripts/Jerry.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Jerry : MonoBehaviour {
 
-    public TextAction textActionPrefab;
+    public TextObject textObjectPrefab;
+    public TextObject textObject;
     public bool alive = true;
     Movement movementScript;
     Animator anim;
@@ -24,11 +25,13 @@ public class Jerry : MonoBehaviour {
         if (coll.gameObject.GetComponent<KillerCarCharacter>())
         {
             Kill();
+            StartCoroutine(WriteText(new Vector3(0, 1, 0), "AGHHHH", TextObject.typeSpeed.INSTANT, 40, 5));
         }
         if (coll.gameObject.GetComponent<Manhole>())
         {
             Kill();
             Destroy(this.GetComponent<SpriteRenderer>());
+            StartCoroutine(WriteText(new Vector3(0, 1, 0), "AAAAAaaaaaa....\n!SPLOOOSH!", TextObject.typeSpeed.INSTANT, 20, 5));
         }
 
     }
@@ -39,14 +42,29 @@ public class Jerry : MonoBehaviour {
         {
             if (coll.gameObject.GetComponent<RoadRubbish>())
             {
+                StartCoroutine(WriteText(new Vector3(0, 1, 0), "This looks like it could easily make a car spin out. Lets get rid of this...", TextObject.typeSpeed.INSTANT, 20, 5));
                 coll.gameObject.GetComponent<RoadRubbish>().Interact();
             }
             if (coll.gameObject.GetComponent<ManholeCover>())
             {
+                StartCoroutine(WriteText(new Vector3(0, 1, 0), "This open manhole looks really dangerous, lets cover this up...", TextObject.typeSpeed.INSTANT, 20, 5));
                 coll.gameObject.GetComponent<ManholeCover>().Interact();
             }
             
         }
+    }
+
+    IEnumerator WriteText(Vector3 offset, string text, TextObject.typeSpeed textSpeed, int textSize, int lifetime)
+    {
+        if (textObject != null)
+        {
+            Destroy(textObject.gameObject);
+        }
+        textObject = Instantiate(textObjectPrefab);
+        textObject.transform.SetParent((FindObjectOfType(typeof(TextManager)) as TextManager).gameObject.transform);
+        textObject.CreateText(this.gameObject, offset, text, textSpeed, textSize);
+        yield return new WaitForSeconds(lifetime);
+        textObject.DeleteText();
     }
 
     void Kill()
--- Assets/Scripts/TextObject.cs
+++ Assets/Scripts/TextObject.cs
@@ -35,10 +35,58 @@ public class TextObject : MonoBehaviour {
         {
             text.text = newText;
         }
+        else if (textSpeed == typeSpeed.QUICK)
+        {
+            StartCoroutine(WriteTextQuickly(text, newText));
+        } else if (textSpeed == typeSpeed.SLOW)
+        {
+            StartCoroutine(WriteTextSlowly(text, newText));
+        }
+    }
+
+    IEnumerator WriteTextQuickly(Text text, string newText)
+    {
+        string currentText = "";
+        string modifiedNewText = newText;
+        while (newText.Length > 0)
+        {
+            currentText = currentText + modifiedNewText.Substring(0, 1);
+            if (modifiedNewText.Length > 1)
+            {
+                modifiedNewText = modifiedNewText.Substring(1, modifiedNewText.Length - 1);
+            } else
+            {
+                modifiedNewText = "";
+            }
+            yield return null;
+            text.text = currentText;
+        }
+        yield return null;
+    }
+
+    IEnumerator WriteTextSlowly(Text text, string newText)
+    {
+        string currentText = "";
+        string modifiedNewText = newText;
+        while (newText.Length > 0)
+        {
+            currentText = currentText + modifiedNewText.Substring(0, 1);
+            if (modifiedNewText.Length > 1)
+            {
+                modifiedNewText = modifiedNewText.Substring(1, modifiedNewText.Length - 1);
+            }
+            else
+            {
+                modifiedNewText = "";
+            }
+            yield return new WaitForSeconds(0.1f);
+            text.text = currentText;
+        }
+        yield return null;
     }
 
     public void DeleteText()
-    {
-        Destroy(this.gameObject);
+    {    
+         Destroy(this.gameObject);
     }
 }
