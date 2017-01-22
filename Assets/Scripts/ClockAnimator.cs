d422c394034772c567cd14b146cdbd75c4eba1b8
Chris O'Grady <cfogrady@gmail.com>
2017-01-22 02:59:28 -0500

    Change clock to use TimeManager

--- Assets/Scripts/ClockAnimator.cs
+++ Assets/Scripts/ClockAnimator.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Assets.Scripts;
 
-public class ClockAnimator : MonoBehaviour {
+public class ClockAnimator : MonoBehaviour, TimeReceiver {
 
     private const float
        hoursToDegrees = 360f / 12f,
@@ -11,10 +12,21 @@ public class ClockAnimator : MonoBehaviour {
        secondsToDegrees = 360f / 60f;
 
     public Transform seconds;
+    public GameObject timeManagerGO;
 
-     void Update()
+    private void Start()
     {
-        DateTime time = DateTime.Now;
-        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        TimeManager manager = timeManagerGO.GetComponent<TimeManager>();
+        manager.addTimeReceiver(this);
+    }
+
+    void Update()
+    {
+    }
+
+    void TimeReceiver.receiveTime(float timeReceived)
+    {
+        //Debug.Log("Received Time " + timeReceived);
+        seconds.localRotation = Quaternion.Euler(0f, 0f, (timeReceived * -secondsToDegrees)%360);
     }
 }
--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour {
 
+    //public delegate void TimeReceiver(float timeReceived);
+
     private float startTime;
     private int tickrate = 1;
-	// Use this for initialization
-	void Start () {
+    private List<TimeReceiver> timeReceivers;
+
+    // Use this for initialization
+    void Start () {
         startTime = Time.time;
         InvokeRepeating("BroadcastCurrentTime", 0.0f, 1.0f);
-	}
+        if (timeReceivers == null) timeReceivers = new List<TimeReceiver>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,7 +25,24 @@ public class TimeManager : MonoBehaviour {
 
     void BroadcastCurrentTime()
     {
-        BroadcastMessage("ReceiveCurrentTime", GetCurrentTime());
+        float currentTime = GetCurrentTime();
+        BroadcastMessage("ReceiveCurrentTime", currentTime);
+        foreach (TimeReceiver receiver in timeReceivers)
+        {
+            receiver.receiveTime(currentTime);
+        }
+    }
+
+    public void addTimeReceiver(TimeReceiver receiver)
+    {
+        if (timeReceivers == null) timeReceivers = new List<TimeReceiver>();
+        this.timeReceivers.Add(receiver);
+    }
+
+    public void removeTimeReceiver(TimeReceiver receiver)
+    {
+        if (timeReceivers == null) timeReceivers = new List<TimeReceiver>();
+        this.timeReceivers.Remove(receiver);
     }
 
     public float GetStartTime()
