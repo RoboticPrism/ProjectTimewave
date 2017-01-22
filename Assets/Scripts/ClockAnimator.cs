c05ba8751b2c632fd1d288b5b2be442aaa97e40a
GAUTAM-PC\Gautam <gautambaghel93@gmail.com>
2017-01-21 23:20:45 -0500

    Map by Gautam

---
+++ Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+    private const float
+       hoursToDegrees = 360f / 12f,
+       minutesToDegrees = 360f / 60f,
+       secondsToDegrees = 360f / 60f;
+
+    public Transform seconds;
+
+     void Update()
+    {
+        DateTime time = DateTime.Now;
+        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    public GameObject inventoryPanel;
+    public GameObject[] inventoryIcons;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (Transform child in inventoryPanel.transform)
+        {
+            if(child.gameObject.tag == collision.gameObject.tag)
+            {
+                string c = child.Find("Text").GetComponent<Text>().text;
+                int tcount = System.Int32.Parse(c) + 1;
+                child.Find("Text").GetComponent<Text>().text = "" + tcount;
+                return;
+            }
+        }
+
+        GameObject i;
+        if(collision.gameObject.tag == "Money")
+        {
+            i = Instantiate(inventoryIcons[0]);
+            i.transform.SetParent(inventoryPanel.transform);
+        }
+        else if (collision.gameObject.tag == "Key")
+        {
+            i = Instantiate(inventoryIcons[1]);
+            i.transform.SetParent(inventoryPanel.transform);
+        }
+        else if (collision.gameObject.tag == "Weapon")
+        {
+            i = Instantiate(inventoryIcons[2]);
+            i.transform.SetParent(inventoryPanel.transform);
+        }
+    }
+}
